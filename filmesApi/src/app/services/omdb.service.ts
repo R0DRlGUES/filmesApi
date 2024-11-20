b255ff9c2bdf66390d7719cb4039a91342673f00
joaovflorisvaldo <joaovflorisvaldo@gmail.com>
2024-11-20 14:20:38 -0300

    Finalizando api

--- filmesApi/src/app/services/omdb.service.ts
+++ filmesApi/src/app/services/omdb.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OmdbService {
   private apiUrl = 'https://www.omdbapi.com/';
@@ -11,7 +11,11 @@ export class OmdbService {
 
   constructor(private http: HttpClient) {}
 
-  getMovies(query: string): Observable<any> {
+  searchMovies(query: string): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}?s=${query}&apikey=${this.apiKey}`);
+  }
+
+  getMovieDetails(id: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}?i=${id}&apikey=${this.apiKey}`);
   }
 }
