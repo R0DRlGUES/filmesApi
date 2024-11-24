96fa31baefe3331d736e6387534595e7f1633d67
joaovflorisvaldo <joaovflorisvaldo@gmail.com>
2024-11-24 20:12:43 -0300

    Fixed Bugs in view, removing comments

--- filmesApi/src/app/movie-details/movie-details.component.ts
+++ filmesApi/src/app/movie-details/movie-details.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-movie-details',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.css'],
 })
 export class MovieDetailsComponent implements OnInit {
   movieId: string | null = null;
-  movie: any = null; // Adicione esta propriedade
+  movie: any = null;
 
   private apiUrl = 'https://www.omdbapi.com/';
   private apiKey = '11037ce9'; // Sua API Key
@@ -18,7 +20,6 @@ export class MovieDetailsComponent implements OnInit {
   constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
   ngOnInit(): void {
-    // Obter o ID do filme da rota
     this.movieId = this.route.snapshot.paramMap.get('id');
     if (this.movieId) {
       this.getMovieDetails(this.movieId);
@@ -30,7 +31,7 @@ export class MovieDetailsComponent implements OnInit {
       .get<any>(`${this.apiUrl}?i=${id}&apikey=${this.apiKey}`)
       .subscribe(
         (response) => {
-          this.movie = response; // Salva os detalhes do filme
+          this.movie = response;
         },
         (error) => {
           console.error('Erro ao buscar detalhes do filme:', error);
